a5b06bc82eab55eeed423cb7d7e4169cbb762281
Armin Ronacher <armin.ronacher@active-4.com>
2018-03-12 23:01:36 +0100

    feat: improved public api and documented it

--- tests/test_de.rs
+++ tests/test_de.rs
@@ -2,6 +2,8 @@
 extern crate serde_derive;
 extern crate serde_plain;
 
+use std::str::FromStr;
+
 #[derive(Deserialize, PartialEq, Eq, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum Test {
@@ -11,6 +13,13 @@ pub enum Test {
 
 #[derive(Deserialize, PartialEq, Eq, Debug)]
 pub struct NewInt(pub i32);
+
+impl FromStr for Test {
+    type Err = serde_plain::Error;
+    fn from_str(value: &str) -> Result<Test, serde_plain::Error> {
+        serde_plain::from_str(value)
+    }
+}
 
 #[test]
 fn test_basics() {
@@ -33,4 +42,9 @@ fn test_basics() {
         Test::BlahBlah
     );
     assert_eq!(serde_plain::from_str::<NewInt>("42").unwrap(), NewInt(42));
+}
+
+#[test]
+fn test_from_str() {
+    assert_eq!("foo_bar_baz".parse::<Test>().unwrap(), Test::FooBarBaz);
 }
--- tests/test_ser.rs
+++ tests/test_ser.rs
@@ -2,6 +2,8 @@
 extern crate serde_derive;
 extern crate serde_plain;
 
+use std::fmt;
+
 #[derive(Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Test {
@@ -11,6 +13,18 @@ pub enum Test {
 
 #[derive(Serialize)]
 pub struct NewInt(i32);
+
+impl fmt::Display for Test {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", serde_plain::to_string(self).unwrap())
+    }
+}
+
+impl ToString for NewInt {
+    fn to_string(&self) -> String {
+        serde_plain::to_string(self).unwrap()
+    }
+}
 
 #[test]
 fn test_basics() {
@@ -28,4 +42,10 @@ fn test_basics() {
     assert_eq!(serde_plain::to_string(&Some(true)).unwrap(), "true");
     assert_eq!(serde_plain::to_string(&None::<()>).unwrap(), "");
     assert_eq!(serde_plain::to_string(&()).unwrap(), "");
+}
+
+#[test]
+fn test_to_string() {
+    assert_eq!(Test::FooBarBaz.to_string(), "foo_bar_baz");
+    assert_eq!(NewInt(42).to_string(), "42");
 }
